1e041a06763442fc2a13c3f719d2c38bb6c6c6fc
Ricardo <ricardodacruz1@hotmail.com>
2023-05-19 00:11:44 -0300

    Game seed

--- src/main/java/com/devricardo/projetoMovie/entities/Game.java
+++ src/main/java/com/devricardo/projetoMovie/entities/Game.java
@@ -19,9 +19,14 @@ public class Game {
 	@Column (name =  "game_year")
 	private Integer year;
 	private  String genre;
-	private String plataform;
+	private String platforms;
+	private Double score;
 	private String  imgUrl;
+	
+	@Column(columnDefinition = "TEXT")// sem isso fica 255 caracter
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	
@@ -30,14 +35,15 @@ public class Game {
 		
 	}
 
-	public Game(long id, String title, Integer year, String genre, String plataform, String imgUrl,
+	public Game(long id, String title, Integer year, String genre, String platforms,Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.plataform = plataform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -76,12 +82,21 @@ public class Game {
 	}
 
 	public String getPlataform() {
-		return plataform;
+		return platforms;
 	}
 
 	public void setPlataform(String plataform) {
-		this.plataform = plataform;
+		this.platforms = plataform;
 	}
+	
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
+	}
+
 
 	public String getImgUrl() {
 		return imgUrl;
