a453bce99809b2d69e216a205fbede60a9026231
Ricardo <ricardodacruz1@hotmail.com>
2023-05-20 01:08:00 -0300

    get game ById

--- src/main/java/com/devricardo/projetoGame/services/GameService.java
+++ src/main/java/com/devricardo/projetoGame/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devricardo.projetoGame.dto.GameDTO;
 import com.devricardo.projetoGame.dto.GameMinDto;
 import com.devricardo.projetoGame.entities.Game;
 import com.devricardo.projetoGame.repositories.GameRepository;
@@ -14,6 +16,14 @@ public class GameService {
 	
 	@Autowired
 	private GameRepository gameRepository;
+	@Transactional (readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;
+	}
+	
+	@Transactional (readOnly = true)
 	public List<GameMinDto> findAll(){
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDto> dto = result.stream().map(x -> new GameMinDto(x)).toList();
